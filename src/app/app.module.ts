55a9c43524e2b59ebd0ccfccb234adbe9dcc99b6
Adil Qarboua <mcadilqarboua@gmail.com>
2023-11-04 19:56:13 +0100

    added pagination to home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolBarComponent } from './tool-bar/tool-bar.component';
@@ -42,7 +42,7 @@ import { MiniProductCardComponent } from './content/side-content/cart-banner/min
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/content/main-content/products-list/products-list.component.ts
+++ src/app/content/main-content/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/product';
+
 
 @Component({
   selector: 'app-products-list',
@@ -6,6 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./products-list.component.css']
 })
 export class ProductsListComponent {
+showPage(_t5: number) {
+
+
+  this.currentProductsShown=this.productsPages.get(_t5);
+}
   products=[
     {
       id: 1,
@@ -188,6 +195,42 @@ export class ProductsListComponent {
       rating: { rate: 3.6, count: 145 }
     }
   ];
+  pagesKeys:number[];
 
+
+  productPerPageModel=6;
+
+
+  pages=((Math.floor(this.products.length/this.productPerPageModel)===this.products.length/this.productPerPageModel)?0:1) +Math.floor(this.products.length/this.productPerPageModel);
+  productsPages:Map<number,Product[]>=new Map();
+  currentProductsShown:Product[]|undefined;
+  
+  constructor(){
+    
+    for(let i=0;i<this.pages;i++){
+    this.productsPages.set(i,this.products.slice(i*this.productPerPageModel,Math.min(this.products.length,(i+1)*this.productPerPageModel)))  
+    console.log(i);
+    console.log(this.productsPages.get(i));
+    console.log(this.pages);
+    this.currentProductsShown=this.productsPages.get(0);
+  }
+  this.pagesKeys=Array.from(this.productsPages.keys());
+  }
+  set productPerPage(ppp:number){
+    this.productPerPageModel=(ppp>2)?ppp:3;
+    this.pages=Math.floor(this.products.length/this.productPerPageModel)+((Math.floor(this.products.length/this.productPerPageModel))===(this.products.length/this.productPerPageModel)?0:1);
+
+    this.productsPages=new Map();
+    for(let i=0;i<this.pages;i++){
+      this.productsPages.set(i,this.products.slice(i*this.productPerPageModel,Math.min(this.products.length,(i+1)*this.productPerPageModel)))  
+      }
+
+      this.pagesKeys=Array.from(this.productsPages.keys());
+
+      console.log(this.productPerPageModel);
+      console.log(ppp);
+    }
+
+    
 
 }
